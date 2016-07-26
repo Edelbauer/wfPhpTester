275291a680732b8728fc9d2816322b46ae82e36c
edelbauer <niklas.edelbauer@gmail.com>
2016-07-26 14:40:48 +0200

    Version Ok, Angular Directives und Router

--- views/login/login.js
+++ views/login/login.js
@@ -5,14 +5,4 @@ var charts;
  */
 charts =  index.controller('Ctrllogin',function ($scope,$log) {
     $log.debug("login Controller wurde geladen");
-
-
-    $scope.Var=10;
-
-    $scope.json = 0;
-
-    $scope.onEnter= function () {
-        alert("Enter");
-    };
-
 });
