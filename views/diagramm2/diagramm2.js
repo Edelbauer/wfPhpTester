9c568cb461de3ae8e93752a4d46c40d4dcf6aa0f
edelbauer <niklas.edelbauer@gmail.com>
2016-07-27 14:43:00 +0200

    Version Ok, Angular Directives und Router

---
+++ views/diagramm2/diagramm2.js
@@ -0,0 +1,10 @@
+"use strict";
+var charts;
+/**
+ * Index Controller
+ */
+charts =  index.controller('Ctrdiagramm2',function ($scope,$log) {
+    $log.debug("diagramm2 Controller wurde geladen");
+});
+
+
