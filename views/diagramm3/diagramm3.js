4b01546706916d83946ab688cd9cf1f0e6c127a3
edelbauer <niklas.edelbauer@gmail.com>
2016-07-27 14:49:44 +0200

    Version Ok, Angular Directives und Router

---
+++ views/diagramm3/diagramm3.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Praktikant.STTINSM on 27.07.2016.
+ */
+"use strict";
+var charts;
+/**
+ * Index Controller
+ */
+charts =  index.controller('Ctrdiagramm3',function ($scope,$log) {
+    $log.debug("diagramm3 Controller wurde geladen");
+});
