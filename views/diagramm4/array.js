7335a8cfd6f87667dabc1021939c4bc6ee01e49b
edelbauer <niklas.edelbauer@gmail.com>
2016-07-28 13:57:53 +0200

    Version Ok, Angular Directives und Router

--- views/diagramm4/array.js
+++ views/diagramm4/array.js
@@ -10,22 +10,3 @@ charts =  index.controller('Ctrdiagramm4',function ($scope,$log) {
     $log.debug("diagramm4 Controller wurde geladen");
 });
 
-var anders = [];
-function arr(){
-    var i;
-    //Die ersten 20 Ziffern werden generiert
-    for (i=0;i<20;i++){
-        anders.push(Math.floor((Math.random() * 10)));
-    }
-    console.log(anders);
-}
-arr();
-
-function arr2(){
-
-    //das 21. Zeichen wird generiert, und das 1. wird abgeschnitten
-    anders.push(Math.floor((Math.random() * 10))) ;
-
-anders.shift();
-console.log(anders);
-}
